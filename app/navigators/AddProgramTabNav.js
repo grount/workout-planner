41f8913283923892ac0bd33c2c0103bbdeb8d321
Daniel Grount <grount1@gmail.com>
2018-03-25 22:32:52 +0300

    Formatted and refactored files

--- app/navigators/AddProgramTabNav.js
+++ app/navigators/AddProgramTabNav.js
@@ -1,28 +1,31 @@
 import AddProgramScreen from '../components/AddProgramScreen.js';
-import { TabNavigator } from 'react-navigation';
+import {TabNavigator} from 'react-navigation';
 
-const addProgramTabNav = TabNavigator({
-	addProg: { 
-		screen: AddProgramScreen,
-		navigationOptions: {
-			title: 'Add Program',
+const addProgramTabNav = TabNavigator(
+	{
+		addProg: {
+			screen: AddProgramScreen,
+			navigationOptions: {
+				title: 'Add Program',
+			},
+		},
+		customize: {screen: AddProgramScreen},
+	},
+	{
+		lazy: true,
+		swipeEnabled: false,
+		tabBarOptions: {
+			style: {
+				backgroundColor: '#05668D',
+			},
+			indicatorStyle: {
+				backgroundColor: '#02C39A',
+			},
+			tabStyle: {
+				height: 40,
+			},
 		},
 	},
-	customize: { screen: AddProgramScreen },
-}, {
-	lazy: true,
-	swipeEnabled: false,
-	tabBarOptions: {
-		style: {
-			backgroundColor: '#05668D',
-		},
-		indicatorStyle: {
-			backgroundColor: '#02C39A'
-		},
-		tabStyle: {
-			height: 40,
-		}
-	},
-})
+);
 
 export default addProgramTabNav;
--- app/navigators/TabNavigator.js
+++ app/navigators/TabNavigator.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { addNavigationHelpers, TabNavigator } from 'react-navigation';
-import { addListener } from '../utils/redux';
+import {connect} from 'react-redux';
+import {addNavigationHelpers, TabNavigator} from 'react-navigation';
+import {addListener} from '../utils/redux';
 import MainScreen from '../components/MainScreen.js';
 import AddProgram from '../components/AddProgramScreen.js';
 
-export const TabNav = TabNavigator({
-	homeScreen: { screen: MainScreen },
-	tempScreen: { screen: AddProgram,
-		navigationOptions: { title: 'Log'}}, // TODO Remove after test screen not needed.
-},{
-	tabBarPosition: 'bottom',
-	lazy: true,
-	tabBarOptions: {
-		style: {
-			backgroundColor: '#05668D',
+export const TabNav = TabNavigator(
+	{
+		homeScreen: {screen: MainScreen},
+		tempScreen: {
+			screen: AddProgram,
+			navigationOptions: {title: 'Log'},
+		}, // TODO Remove after test screen not needed.
+	},
+	{
+		tabBarPosition: 'bottom',
+		lazy: true,
+		tabBarOptions: {
+			style: {
+				backgroundColor: '#05668D',
+			},
+			indicatorStyle: {
+				backgroundColor: '#02C39A',
+			},
+			tabStyle: {
+				height: 40,
+			},
 		},
-		indicatorStyle: {
-			backgroundColor: '#02C39A'
-		},
-		tabStyle: {
-			height: 40,
-		}
-	}
-});
+	},
+);
 
 class tabWithNavigator extends React.Component {
 	render() {
-		const { dispatch, nav } = this.props;
+		const {dispatch, nav} = this.props;
 		return (
 			<TabNav
 				navigation={addNavigationHelpers({
@@ -42,7 +46,7 @@ class tabWithNavigator extends React.Component {
 }
 
 const mapStateToProps = state => ({
-	nav: state.homeTabs
+	nav: state.homeTabs,
 });
 
 export default connect(mapStateToProps)(tabWithNavigator);
