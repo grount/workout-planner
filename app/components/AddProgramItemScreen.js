370894ffd6085db845aeb20913e889289e6bcea7
Daniel Grount <grount1@gmail.com>
2018-03-27 19:17:21 +0300

    Added toolbar with navigation

--- app/components/AddProgramItemScreen.js
+++ app/components/AddProgramItemScreen.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ThemeProvider, Toolbar} from 'react-native-material-ui';
+import {Text, Body, Card, CardItem, Content, Container} from 'native-base';
 
 export default class AddProgramItemScreen extends React.Component {
+	static navigationOptions = ({navigation}) => ({
+		header: (
+			<ThemeProvider>
+				<Toolbar
+					leftElement="arrow-back"
+					onLeftElementPress={() => navigation.goBack()}
+					centerElement="Options"
+					rightElement="save"
+					onRightElementPress={() => null}
+				/>
+			</ThemeProvider>
+		),
+	});
+
 	render() {
 		return (
-			<View>
-				<Text> Options </Text>
-			</View>
+			<Container>
+				<Content>
+					<Card>
+						<CardItem header>
+							<Text>Workout Configurations</Text>
+						</CardItem>
+						<CardItem>
+							<Body>
+								<Text> Test </Text>
+							</Body>
+						</CardItem>
+					</Card>
+				</Content>
+			</Container>
 		);
 	}
 }
