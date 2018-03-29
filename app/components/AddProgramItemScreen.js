abb5efcef84277a3a3f68db74c10bd10db03cf25
Daniel Grount <grount1@gmail.com>
2018-03-29 17:43:40 +0300

    Removed react-native-numeric-input for react-native TextInput

--- app/components/AddProgramItemScreen.js
+++ app/components/AddProgramItemScreen.js
@@ -1,19 +1,16 @@
 import React from 'react';
+import {TextInput} from 'react-native';
 import {ThemeProvider, Toolbar} from 'react-native-material-ui';
-import {
-	Header,
-	Text,
-	Body,
-	Card,
-	CardItem,
-	Content,
-	Container,
-} from 'native-base';
-import NumericInput, {calcSize} from 'react-native-numeric-input';
+import {Text, Body, Card, CardItem, Content, Container} from 'native-base';
 
 export default class AddProgramItemScreen extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			repetitions: 1,
+			sets: 1,
+		};
 	}
 	static navigationOptions = ({navigation}) => ({
 		header: (
@@ -29,17 +26,29 @@ export default class AddProgramItemScreen extends React.Component {
 		),
 	});
 
-	renderNumericInput() {
-		return (
-			<NumericInput
-				totalWidth={calcSize(140)}
-				totalHeight={calcSize(60)}
-				onChange={console.log('123')}
-				textColor="#03C39A"
-				iconStyle={{color: '#05668D'}}
-				rounded
-			/>
-		);
+	onChangeSetInput(text) {
+		const newText = this.onChangeInput(text);
+		this.setState({sets: newText});
+		console.log(this.state);
+	}
+
+	onChangeRepetitionsInput(text) {
+		const newText = this.onChangeInput(text);
+		this.setState({repetitions: newText});
+	}
+
+	onChangeInput(text) {
+		let newText = '';
+		let numbers = '0123456789';
+
+		for (var i = 0; i < text.length; i++) {
+			if (numbers.indexOf(text[i]) > -1) {
+				newText = newText + text[i];
+			} else {
+				alert('please enter numbers only');
+			}
+		}
+		return newText;
 	}
 
 	render() {
@@ -56,13 +65,26 @@ export default class AddProgramItemScreen extends React.Component {
 						<CardItem>
 							<Body>
 								<Text> Sets: </Text>
-								{this.renderNumericInput()}
+								<TextInput
+									style={{
+										padding: 2,
+										marginLeft: 5,
+										borderWidth: 0.5,
+										width: 30,
+										height: 30,
+										borderRadius: 4,
+										borderColor: '#05668D',
+									}}
+									keyboardType="numeric"
+									value={this.state.sets}
+									maxLength={3}
+									onChangeText={text => this.onChangeSetInput(text)}
+								/>
 							</Body>
 						</CardItem>
 						<CardItem>
 							<Body>
 								<Text> Repetitions: </Text>
-								{this.renderNumericInput()}
 							</Body>
 						</CardItem>
 					</Card>
