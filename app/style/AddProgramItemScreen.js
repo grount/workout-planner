fd648d65a4d6e3bc630129e833c3b4426410276c
Daniel Grount <grount1@gmail.com>
2018-03-31 13:01:07 +0300

    Added sets and reps styles

---
+++ app/style/AddProgramItemScreen.js
@@ -0,0 +1,15 @@
+import MainStyles from '../style/MainStyles.js';
+
+const style = {
+	textInput: {
+		padding: 2,
+		marginLeft: 5,
+		borderWidth: 0.5,
+		width: 30,
+		height: 30,
+		borderRadius: 4,
+		borderColor: MainStyles.blue,
+	},
+};
+
+export default style;
